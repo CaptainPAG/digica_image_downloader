5d76c8c5356cf4daa35cd2f8d8d9e6d170a79860
CaptainPAG <line.6th818@gmail.com>
2023-09-21 16:45:06 +0900

    アプリ起動終了後に生成ディレクトリを表示する

--- application.py
+++ application.py
@@ -2,7 +2,8 @@ import tkinter as t
 import tkinter.messagebox as mb
 import main
 import subprocess
-
+from config import *
+import os
 
 root = t.Tk()
 
@@ -21,7 +22,7 @@ def submit():
         # 配列の1つ目にｎ不要なテキストがい含まれているので削除する
         del list[0]
         main.main(list=list)
-        path = r"C:\Users\line6\my_services\digica_image_downloader\cards"
+        path = fr"{os.getcwd()}\cards"
         subprocess.Popen(['explorer', path], shell=True)
 
 root.title = "Deck Image Transfer"
@@ -29,6 +30,7 @@ root.title = "Deck Image Transfer"
 x, y = 300, 400
 root.minsize(y, x)
 root.geometry(f"{y}x{x}")
+root.title("digica image downloader")
 
 label = t.Label(root, text="IMPORT text by generated Deck Builder")
 label.pack(pady=10, padx=10)
@@ -39,7 +41,7 @@ entiry.pack()
 button = t.Button(root, text="submit", command=submit)
 button.pack(pady=10)
 
-result_l = t.Label(root, text="", wraplength=200)
+result_l = t.Label(root, text=os.getcwd(), wraplength=200)
 result_l.pack()
 
 root.mainloop()
